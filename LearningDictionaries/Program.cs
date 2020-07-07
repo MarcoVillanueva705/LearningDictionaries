044a4d8947e93a538045a9ca9ec9420034b8c544
marco villanueva <villanuevamarco98@gmail.com>
2020-07-06 20:53:16 -0600

    Add project files.

---
+++ LearningDictionaries/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LearningDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> config = new Dictionary<string, string>();
+            config.Add("resolution", "1920x1080");
+            config.Add("title", "MyWebsite");
+
+            Console.WriteLine(config["title"]);
+
+            foreach (var setting in config)
+            {
+                //setting.Value is each value in a key/value pair
+                Console.WriteLine(setting.Value);
+            }
+        }
+    }
+}
